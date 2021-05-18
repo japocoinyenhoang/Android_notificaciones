f1a05c887c5149f24bee857933a866c09803e5a2
japocoinyenhoang <japocoin@gmail.com>
2021-05-18 18:45:01 +0200

    permissions and firsebase connection added

---
+++ app/src/main/java/com/japocoin/notificaciones/MyFirebaseMessagingService.java
@@ -0,0 +1,15 @@
+package com.japocoin.notificaciones;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+    }
+}
